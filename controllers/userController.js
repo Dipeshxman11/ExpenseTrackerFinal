74e71c7a9054da836fc93640009b6fc925f9b8a9
Dipeshxman11 <dipeshramane9378>
2023-08-01 03:28:21 +0530

    Using bcrypt for security of password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 exports.getLoginPage = (req, res, next) => {
     res.sendFile(path.join(__dirname, "../", "public", "views", "login.html"));
@@ -9,22 +10,22 @@ exports.postUserSignUp = (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
+
     User.findOne({ where: { email: email } })
         .then((user) => {
             if (user) {
-                res.send(
-                    `<script>alert('This email is already taken. Please choose another one.'); window.location.href='/'</script>`
-                );
                 res
                     .status(409)
                     .send(
                         `<script>alert('This email is already taken. Please choose another one.'); window.location.href='/'</script>`
                     );
             } else {
-                User.create({
-                    name: name,
-                    email: email,
-                    password: password,
+                bcrypt.hash(password, 10, async (err, hash) => {
+                    await User.create({
+                        name: name,
+                        email: email,
+                        password: hash,
+                    });
                 });
                 res
                     .status(200)
@@ -36,25 +37,35 @@ exports.postUserSignUp = (req, res, next) => {
         .catch((err) => console.log(err));
 };
 
+
 exports.postUserLogin = (req, res, next) => {
     const email = req.body.loginEmail;
     const password = req.body.loginPassword;
 
     User.findOne({ where: { email: email } }).then((user) => {
         if (user) {
-            if (user.password == password) {
-                res
-                    .status(200)
-                    .send(
-                        `<script>alert('Login Successful!'); window.location.href='/'</script>`
-                    );
-            } else {
-                res
-                    .status(401)
-                    .send(
-                        `<script>alert('Password Incorrect!'); window.location.href='/'</script>`
-                    );
-            }
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (err) {
+                    res
+                        .status(500)
+                        .send(
+                            `<script>alert('Something went wrong!'); window.location.href='/'</script>`
+                        );
+                }
+                if (result === true) {
+                    res
+                        .status(200)
+                        .send(
+                            `<script>alert('Login Successful!'); window.location.href='/'</script>`
+                        );
+                } else {
+                    res
+                        .status(401)
+                        .send(
+                            `<script>alert('Password Incorrect!'); window.location.href='/'</script>`
+                        );
+                }
+            });
         } else {
             res
                 .status(404)
